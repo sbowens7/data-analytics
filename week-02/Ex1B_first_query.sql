d34a6f858e90c98fcdf0b2838a4a71f175ae7af8
shaquan bowens <sbowens@my.yearupunited.org>
2026-04-20 10:01:42 -0500

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 Sakila is a database used to collect information about movies and tv shows for use by producers of the production
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrived 200 records
+
+ SELECT * FROM film; -- Retrived 100 records
