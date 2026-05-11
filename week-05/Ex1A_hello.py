eed0432f683f6c003feb02325536022c94050b7e
Shaquan Bowens <sbowens@my.yearupunited.org>
2026-05-11 03:47:15 -0500

    1A

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,16 @@
 print('Hello world!')
+message = 'Hello World'
+print(message)
+# "Hello World" prints twice because ...
+
+# Displaying dollars and cents
+dollars = 3
+cents = .5
+print(dollars + cents)
+# The two variables add up together rather than being a string where the two numbers woulde be next to each other
+cents = cents + .25
+print(dollars + cents)
+# 3.75
+d_str = ' 3 dollars'
+c_str = '50 cents' 
+print( d_str + " " + c_str)
